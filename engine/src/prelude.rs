5153bae3d4b9f985381b11c9bfeeb8ab8a440175
peanutbother <peanutbother@proton.me>
2024-04-29 23:12:23 +0200

    move macros into own crate; future: added proc_macros

--- engine/src/prelude.rs
+++ engine/src/prelude.rs
@@ -9,8 +9,4 @@ pub use bevy_asset_loader::prelude::*;
 pub use bevy_ecs_ldtk::prelude::*;
 
 // macros
-pub use super::invert;
-pub use super::plugin;
-pub use super::register_entity;
-pub use super::register_int_cell;
-pub use super::register_types;
+pub use macros::*;
